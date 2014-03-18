cddc3e67eb45cf246589c1fad5cc1a6f2ad92fbf
Alex Kost <alezost@gmail.com>
2014-03-18 23:35:22 +0400

    Update version (0.6) and package commentary

--- alect-themes.el
+++ alect-themes.el
@@ -1,10 +1,10 @@
-;;; alect-themes.el --- Configurable color themes for Emacs 24 or later
+;;; alect-themes.el --- Configurable light, dark and black themes for Emacs 24 or later
 
 ;; Copyright (C) 2013-2014 Alex Kost
 
 ;; Author: Alex Kost <alezost@gmail.com>
 ;; Created: 10 Jul 2013
-;; Version: 0.5.1
+;; Version: 0.6
 ;; Package-Requires: ((emacs "24.0"))
 ;; URL: http://github.com/alezost/alect-themes
 ;; Keywords: color theme
@@ -27,15 +27,17 @@
 ;; This package provides 6 highly customizable color themes (light, dark
 ;; and black) for GNU Emacs 24 or later.  These themes are intended to
 ;; be used with GUI, so only graphical terminals are supported by
-;; default.  However you can "enable" the themes for other terminals
-;; with `alect-display-class' variable.
+;; default.  However you can "enable" the themes for other classes of
+;; terminals with `alect-display-class' variable.
 
 ;; You can install the package from MELPA.  If you prefer the manual
 ;; installation, put these lines into your init-file:
+;;
 ;;   (add-to-list 'load-path "/path/to/alect-themes")
 ;;   (add-to-list 'custom-theme-load-path "/path/to/alect-themes")
 
 ;; If you also want to enable a theme on Emacs start, use this:
+;;
 ;;   (load-theme 'alect-light t)
 
 ;; Ways for configuring the themes:
@@ -45,10 +47,14 @@
 ;;    redefine the whole variable (see the code) or `alect-set-color' to
 ;;    modify specified colors.
 ;;
-;; 2. Overriding face specification.  You can change the look of
+;; 2. Ignoring faces and variables.  You can force alect-themes not to
+;;    modify some (or all) faces and variables with
+;;    `alect-ignored-faces' and `alect-ignored-variables' variables.
+;;
+;; 3. Overriding face specification.  You can change the look of
 ;;    particular faces by setting `alect-overriding-faces' variable.
 ;;
-;; 3. Above that, the inverted color themes (`alect-light-alt' and
+;; 4. Above that, the inverted color themes (`alect-light-alt' and
 ;;    `alect-dark-alt') can be configured with
 ;;    `alect-inverted-color-regexp' variable.
 
