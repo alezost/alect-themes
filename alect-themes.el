b1f97e4bc0dc6ec91c7e9999fbe9fa371016463b
Alex Kost <alezost@gmail.com>
2025-12-05 18:03:25 +0300

    Add 'emoji' faces

--- alect-themes.el
+++ alect-themes.el
@@ -795,6 +795,12 @@ For INVERT, see `alect-get-color'."
          (emms-stream-name-face       ((,c :foreground ,(gc 'blue+1))))
          (emms-stream-url-face        ((,c :inherit default)))
 
+         ;; emoji
+         (emoji-list-header        ((,c :inherit alect-title)))
+         (emoji                    ((,c :height 2.5)))
+         (emoji-with-derivations   ((,c :inherit emoji
+                                        :background ,(gc 'bg))))
+
          ;; erc
          (erc-header-line          ((,c :inherit header-line)))
          (erc-bold-face            ((,c :weight bold)))
